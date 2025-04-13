5ad54948b62ad71964ae53f4dde489e95a00ce1e
Trainboy15 <162745871+Trainboy15@users.noreply.github.com>
2025-04-13 18:59:04 -0400

    Create Bow.java

---
+++ Game/src/Bow.java
@@ -0,0 +1,12 @@
+public class Bow {
+  String name = "Sword";
+  int sellPrice = 15;
+  int buyPrice = 20;
+  int damage = 15;
+  int damageMulti = 1;
+  String isMagic = "False";
+  String isWeapon = "True";
+  int kills = 0;
+  int durability = 1000;
+  String nickName = "";
+}
