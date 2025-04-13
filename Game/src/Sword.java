f7804e72d2b4da6aebbb9af4b2a42213c7ef9106
Trainboy15 <162745871+Trainboy15@users.noreply.github.com>
2025-04-13 18:58:47 -0400

    Create Sword.java

---
+++ Game/src/Sword.java
@@ -0,0 +1,12 @@
+public class Sword {
+  static String name = "Sword";
+  int sellPrice = 15;
+  int buyPrice = 15;
+  static int damage = 10;
+  int damageMulti = 1;
+  String isMagic = "False";
+  String isWeapon = "True";
+  int kills = 0;
+  int durability = 1000;
+  static String nickName = "";
+}
